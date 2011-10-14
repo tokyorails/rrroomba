76d7f5c1ee9605dd2b55fd146f4d6338119ed509
charles <charles@cookpad.com>
2011-10-14 15:24:20 +0900

    blank project, a supporting doc and notes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rrroomba::Application.config.secret_token = '55afe9bc3352702bf59165981182873bd3a9216a2891adf61f54965fa70c24e829ec5070b2e6fb09214ac5b9f044ed6acc4e58eec15535725b90f61986883551'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rrroomba::Application.config.session_store :cookie_store, :key => '_rrroomba_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rrroomba::Application.config.session_store :active_record_store
